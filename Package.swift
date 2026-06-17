4299de0fa4a71e40d11861c1a59dffcf4c8ca897
Kyle Yoon <yoonkylesejin@gmail.com>
2026-06-17 09:21:01 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FilterChain",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FilterChain",
+            targets: ["FilterChain"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FilterChain"
+        ),
+        .testTarget(
+            name: "FilterChainTests",
+            dependencies: ["FilterChain"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
